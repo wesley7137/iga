ccffb98d1a8d75d9d13c03222d04b2cd1e8adc91
Jay Ram <jaycram@gmail.com>
2023-07-29 14:52:25 -0700

    added claude demo

---
+++ claude.py
@@ -0,0 +1,38 @@
+from flask import Flask, jsonify, request
+from dotenv import load_dotenv
+from langchain.chat_models import ChatAnthropic
+from langchain.chains import ConversationChain
+from langchain.agents import Tool
+from langchain.agents import AgentType
+from langchain.utilities import GoogleSerperAPIWrapper
+from langchain.agents import initialize_agent
+from langchain.memory import ConversationBufferMemory
+from langchain.prompts.chat import (
+    ChatPromptTemplate,
+    MessagesPlaceholder,
+    SystemMessagePromptTemplate,
+    AIMessagePromptTemplate,
+    HumanMessagePromptTemplate,
+)
+
+load_dotenv()
+
+data = request.get_json()
+
+
+llm = ChatAnthropic()
+search = GoogleSerperAPIWrapper()
+tools = [
+    Tool(
+        name="Intermediate Answer",
+        func=search.run,
+        description="useful for when you need to ask with search",
+    )
+]
+
+self_ask_with_search = initialize_agent(
+    tools, llm, agent=AgentType.SELF_ASK_WITH_SEARCH, verbose=True
+)
+self_ask_with_search.run(
+    "What is the hometown of the reigning men's U.S. Open champion?"
+)
