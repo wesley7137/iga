f9149fac04ca455a052e5fddb81219f085a86083
Jay Ram <jaycram@gmail.com>
2023-07-30 06:36:50 -0700

    rewrote using https://github.com/langchain-ai/langchain/issues/6925

--- api/claude.py
+++ api/claude.py
@@ -24,6 +24,8 @@ from langchain.tools.file_management import (
     ListDirectoryTool,
 )
 import wget
+from AnthropicAgent import AnthropicAgent
+from langchain.agents import Tool, AgentExecutor, BaseSingleActionAgent
 
 load_dotenv()
 
@@ -82,14 +84,36 @@ def query_claude(request) -> str:
             description="useful for when you need to ask with search",
         )
     ]
+    agent_instructions = """You are a helpful assistant. Help the user answer any questions.
 
-    agent = initialize_agent(
-        tools, llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True, max_iterations=4
-    )
+    You have access to the following tools:
+
+    {tools}
+
+    In order to use a tool, you can use <tool></tool> and <tool_input></tool_input> tags. \
+    You will then get back a response in the form <observation></observation>
+    For example, if you have a tool called 'search' that could run a google search, in order to search for the weather in SF you would respond:
+
+    <tool>search</tool><tool_input>weather in SF</tool_input>
+    <observation>64 degrees</observation>
+
+    When you are done, respond with a final answer between <final_answer></final_answer>. For example:
+
+    <final_answer>The weather in SF is 64 degrees</final_answer>
+
+    Begin!
+
+    Question: {question}"""
+    model = ChatAnthropic(model="claude-2")
+    prompt_template = ChatPromptTemplate.from_template(agent_instructions) + AIMessagePromptTemplate.from_template("{intermediate_steps}")
+    chain = prompt_template | model.bind(stop=["</tool_input>", "</final_answer>"])
     
-    for step in agent.iter(request):
+    agent = AnthropicAgent(tools=tool_list, chain=chain)
+    agent_executor = AgentExecutor(agent=agent, tools=tool_list, verbose=True)
+       
+    for step in agent_executor.iter(request):
+        print(step)
         if output := step.get("intermediate_step"):
             action, value = output[0]
-            print(f"{action}: {value}")
-    # response = self_ask_with_search.run(request)
+
     return value
