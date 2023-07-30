8b24dbb4a5a15426696a05e84d10a501b0d67d0f
Jay Ram <jaycram@gmail.com>
2023-07-29 21:45:47 -0700

    made lambda

--- tools/get_search_tool.py
+++ tools/get_search_tool.py
@@ -4,10 +4,10 @@ from langchain.utilities import GoogleSerperAPIWrapper
 
 def get_search_tool():
     search = GoogleSerperAPIWrapper()
-    
+
     return Tool(
         name="search",
-        func=search.run,
+        func=lambda x : search.run(x),
         description="useful for when you need to ask with search (especially for a research paper)",
     )
         
